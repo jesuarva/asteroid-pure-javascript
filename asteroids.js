e01d28ee0410639de897d6561b328493b0ea08b9
jesuarva <jesuarva.developer@gmail.com>
2018-07-02 22:24:24 +0200

    FIX VARIABLES CONFLICT

--- asteroids.js
+++ asteroids.js
@@ -37,11 +37,11 @@ class Game extends Canvas {
  * LEVEL
  */
 class Level {
-  constructor(levelNumber, startingNumerAsteroids, numberofEnemyShips, asteroidBaseSpeed) {
-    const levelNumber = levelNumber;
-    const startingNumerAsteroids = startingNumerAsteroids;
-    const numberofEnemyShips = numberofEnemyShips;
-    const asteroidBaseSpeed = asteroidBaseSpeed;
+  constructor(_levelNumber, _startingNumerAsteroids, _numberofEnemyShips, _asteroidBaseSpeed) {
+    const levelNumber = _levelNumber;
+    const startingNumerAsteroids = _startingNumerAsteroids;
+    const numberofEnemyShips = _numberofEnemyShips;
+    const asteroidBaseSpeed = _asteroidBaseSpeed;
   }
 }
 
